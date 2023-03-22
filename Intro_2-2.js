f42f9c49ec194547677435fd5673e6e8aac32fa8
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-03-22 13:35:15 +0100

    JS-Intro

---
+++ Intro_2-2.js
@@ -0,0 +1,146 @@
+// INTRODUZIONE 2-1 A JAVASCRIPT
+
+// Incrementi:
+
+let index = 5;
+
+// index = index + 5;
+index += 5;
+
+let counter = 0;
+
+// counter = counter + 1;
+// counter += 1;
+counter++;
+
+let paperino = 5;
+
+console.log(paperino++); // Il console.log() prima stampa il valore, poi lo incrementa.
+console.log(paperino++); // Quindi prima si vedrà 5, poi 6 e infine varrà 7.
+console.log(paperino);
+
+let pluto = 5;
+
+console.log(++pluto);  // In questo caso avviene il contrario, perciò il valore viene 
+console.log(++pluto);  // prima incrementato, poi verrà stampato. 
+console.log(pluto);  // Quindi si vedrà prima 6, poi 7 ed infine di nuovo 7.
+
+//IF, ELSE, ELSE IF:
+
+let astro = true;
+
+if (astro) {                    // IF fa quello che è vero, 
+    console.log('var merda');   // ELSE fa quello che è falso.
+} else {                        // Se astro fosse false, 
+    console.log('viva var');    // IF eseguirebbe il falso ed ELSE il vero.
+};
+
+let chueke = 3;
+
+if (chueke !== 3) {  // Se chueke fosse diverso da 3, allora verrebbe loggato...
+    console.log('Viva JavaScript!');  // ...Viva JavaScript!
+};
+
+let trell = 5;
+
+if (trell === 3) {
+    console.log('Qui');
+} else if (trell === 5) {
+    console.log('Quo');
+} else if (trell === 1) {
+    console.log('Qua');
+} else {
+    console.log("'Sti cazzi!");
+};
+
+let bubuCrocco = 5;
+
+if (bubuCrocco < 10) {  // In questo caso verrà stampato 'Che gnocca!',
+    console.log('Che gnocca!');  // perché l'IF si ferma già alla prima condizione,
+} else if (bubuCrocco < 6) {  // essendo già vera.
+    console.log('Vorrei fosse seduta sul mio...');
+} else {
+    console.log('...maledetta....');
+};
+
+// Ciclo WHILE:
+
+/* while (true) {
+    console.log('Ciao.');
+}*/ // Questo è un cilco infinito, perchè il WHILE lo esegue se è vero (true);
+
+let checco = 0;
+
+while (checco < 10) {  // Questo programma stamperà da 1 a 10,
+    checco++;          // perchè prima incrementa, poi logga (di fatti si ferma
+    console.log(checco);  // a 9 ma viene ulteriormente incrementato alla fine.
+}; 
+
+while (checco > -1) {
+    checco--;
+    console.log(checco);
+};
+
+// Ciclo FOR:
+
+for (let ftchh = 0; ftchh < 10; ftchh++) {  // Col ciclo FOR si mette al suo interno
+    console.log('...tua moglie!');  // la variabile, seguita dalla condizionale
+};                                  // e infine dall'incremento.
+
+for (let elisBubu = 5; elisBubu < 100; elisBubu += 5) {  // Questo programma stamperà
+    console.log(elisBubu);                               // tutti i multipli di 5
+};                                                       // da 5 a 100.
+
+// ESERCIZI:
+
+// Stampare tutti i numeri "dispari" minori di 300:
+
+/*let poldo = 1;
+
+while (poldo < 300) {
+    console.log(poldo);
+    poldo+=2;
+}*/
+
+// Stampare i numeri da 10 a 0 e viceversa:
+
+/*let cipolle = 0;
+let patate = 11;
+
+while (cipolle <= 20) {
+
+    if (cipolle <= 10) {
+        patate--;
+    } else {
+        patate++;
+    }
+    
+    cipolle++;
+    console.log(patate);
+
+}*/
+
+let puffo = 10;
+let step = -1;
+
+while (puffo <= 10) {
+
+    console.log(puffo);
+
+    if (puffo === 0) {
+        step = 1;
+    };
+
+    puffo += step;
+
+};
+
+// Cicli annidati:
+
+for (let x = 1; x < 11; x++) {
+    
+    for (let y = 1; y < 11; y++) {
+        console.log("X: ", x, "Y: ", y);
+    };
+    
+};
--- esercizi.js
+++ esercizi.js
@@ -1,11 +1,28 @@
-let kedek = "#";
+// ESERCIZIO 1
+
+// Soluzione mia:
+
+/*let canc = "#";
 
 for (let lel = 1; lel <= 7; lel++) {
-    console.log(kedek);
-    kedek += "#";
-};
+    console.log(canc);
+    canc += '#';
+};*/
 
-let number = 1;
+// Soluzione in classe:
+
+/*let string = " ";
+
+while (string.length < 7) {
+    string += "#";
+    console.log(string);
+};*/
+
+
+
+// ESERCIZIO 2:
+
+//let number = 1;
 
 /*for (let result = 0; result < 100; result++) {
 
@@ -21,7 +38,9 @@ let number = 1;
 
 };*/
 
-for (let result = 0; result < 100; result++) {
+// Soluzione mia:
+
+/*for (let result = 0; result < 100; result++) {
 
     if (number % 3 === 0 && number % 5 === 0) {
         console.log('FizzBuzz');
@@ -35,8 +54,52 @@ for (let result = 0; result < 100; result++) {
     
     number += 1;
 
-};
+};*/
 
+// Soluzione mia ripulita in classe 
+// (senza variabile "numb" ma usando direttamente "loop", sostituito da "i")
+// e le ulteriori condizioni tolte perché inutili:
+
+
+/*for (let i = 1; i < 101; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0){
+        console.log('Fizz')
+    } else {
+        console.log(i);
+    }
+    
+};*/
+
+// Soluzione alternativa in classe:
+
+// for (let i = 0; i < 101; i++) {
+    
+//     let result = "";  // Le stringhe vuote sono convertite in "false" (è un booleano).
+
+//     if (i % 3 === 0) {
+//         result += 'Fizz';
+//     }; 
+//     if (i % 5 === 0) {
+//         result += 'Buzz';
+//     }; 
+//     if (result) {  // Se la stringa è piena è "true", quindi esegue l'IF,
+//         console.log(result);  //  viene stampata ricomincia da capo con "continue" (non stampando "i").
+//         continue; // "else è la stessa cosa di continue".
+//     };
+
+//     console.log(i);
+    
+// };
+
+
+// ESERCIZIO 3:
+
+// Soluzione mia:
 
 for (let loop = 1; loop <= 8; loop++) {
 
@@ -46,13 +109,4 @@ for (let loop = 1; loop <= 8; loop++) {
         console.log(("#" + " ").repeat(4));
     };
 
-};
-
-let nome = prompt('Digita il tuo nome:');
-let cognome = prompt('Digita il tuo cognome:');
-
-if (nome === "" || cognome === "") {
-    alert('Dati non validi!');
-} else {
-    alert('Il tuo nominativo è: ' + nome + " " + cognome);
 };
